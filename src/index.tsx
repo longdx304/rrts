c6114b85d469a8588859a1b7416e450f35c42ee8
Long Do <longdo@dobtech.vn>
2022-03-24 13:01:52 +0700

    using component function

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,43 @@
-import React from "react";
 import ReactDOM from 'react-dom';
 
 interface AppProps {
   color?: string;
 }
 
-interface AppState {
-  counter: number;
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>
 }
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
 
-    this.state = { counter: 0 };
-  }
+// interface AppState {
+//   counter: number;
+// }
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 })
-  }
+//     this.state = { counter: 0 };
+//   }
 
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 })
-  }
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 })
+//   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    )
-  }
-}
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     )
+//   }
+// }
 
 ReactDOM.render(
-  <App />,
+  <App color="red" />,
   document.querySelector('#root')
 )
