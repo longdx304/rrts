0d2c7f1c96dd7b9a63a28652f11108c9a33b2610
Long Do <longdo@dobtech.vn>
2022-03-24 13:00:03 +0700

    mange state using constructor

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,16 @@ import ReactDOM from 'react-dom';
 interface AppProps {
   color?: string;
 }
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+
+interface AppState {
+  counter: number;
+}
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { counter: 0 };
+  }
 
   onIncrement = (): void => {
     this.setState({ counter: this.state.counter + 1 })
