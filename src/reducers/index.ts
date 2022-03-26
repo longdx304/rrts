c542627343a57fe459a2709131dc7229b6631ea4
Long Do <longdo@dobtech.vn>
2022-03-26 16:49:48 +0700

    add todo reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { Todo } from '../actions';
+import { todosReducer } from './todo';
 
-export const reducers = combineReducers({
-  counter: () => 1,
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer,
 });
